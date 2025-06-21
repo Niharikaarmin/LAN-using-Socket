1240be63185ed06ddab0ccc42dbeaa3d3ca54185
Niharikaarmin <niharikaarmin@gmail.com>
2025-06-21 11:16:05 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,23 +1,23 @@
 import socket
 
-# Replace with server's IP address
-server_ip = input("Enter Server IP: ")  # eg: 192.168.0.105 same server kum client same ip irruntha matum thaan run akum code a
+
+server_ip = input("Enter Server IP: ")
 port = 12345
 
-# Create socket
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)#socket.af_inet-ip address,socket.sock_stream-tcp
-client_socket.connect((server_ip, port))#connect pannu (server_ip-ip address .port-port number)
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((server_ip, port))
 
 print(f"[CONNECTED] Connected to server {server_ip}:{port}")
 
-# Communicate
+
 while True:
     msg = input("You: ")
-    client_socket.send(msg.encode())#msg send panrom sencodela  client to server ku
+    client_socket.send(msg.encode())
     if msg.lower() == 'exit':
         break
-    data = client_socket.recv(1024).decode()#server la vantha message receieve panrom
+    data = client_socket.recv(1024).decode()
     print(f"Server: {data}")
 
-# Close connection
+
 client_socket.close()
